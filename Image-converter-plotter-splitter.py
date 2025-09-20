f419b68445a41e6867447d869b56dbe6f129bad7
Ak23b <athiengmayor@gmail.com>
2025-09-20 08:18:04 +0300

    2-line spacing

--- Image-converter-plotter-splitter.py
+++ Image-converter-plotter-splitter.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 img = Image.open("yourImage.png")
 img_arr = np.array(img)
 
+
 print("Shape:\n",img_arr.shape)
 print("Dimensions:\n",img_arr.ndim)
+
 
 # Plotting the Image
 plt.imshow(img_arr)
@@ -15,24 +17,29 @@ plt.title("Original Image")
 plt.axis("off")
 plt.show()
 
+
 # Converting the Image to grayscale
 gray = img.convert("L")
 gray_arr = np.array(gray)
+
 
 plt.imshow(gray_arr,cmap="Grays")
 plt.title("Gray Image")
 plt.axis("off")
 plt.show()
 
+
 # Splitting into RGB and plotting each color channel
 R = img_arr[:,:,0]
 G = img_arr[:,:,1]
 B = img_arr[:,:,2]
 
+
 plt.imshow(R,cmap="Reds")
 plt.title("Red Channel Image.")
 plt.axis("off")
 plt.show()
+
 
 plt.imshow(G,cmap="Greens")
 plt.title('Green Channel')
